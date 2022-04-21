e4fff7da9bc70da0f7e481d4039dfe0e0f71e8ae
Edoardo Cignoni <edoardo.cignoni96@gmail.com>
2022-04-21 11:19:27 +0200

    Added Matern kernels

--- gpx/kernels.py
+++ gpx/kernels.py
@@ -19,13 +19,37 @@ def kmap(kernel, x1, x2, params):
 def squared_exponential_kernel(x1, x2, params):
     z1 = x1 / params["lengthscale"]
     z2 = x2 / params["lengthscale"]
-    return jnp.exp(-jnp.sum(z1 - z2) ** 2)
+    d2 = jnp.sum((z1 - z2) ** 2)
+    return jnp.exp(-d2)
+
+
+def matern12_kernel(x1, x2, params):
+    z1 = x1 / params["lengthscale"]
+    z2 = x2 / params["lengthscale"]
+    d = jnp.sqrt(jnp.sum((z1 - z2) ** 2))
+    return jnp.exp(-d)
+
+
+def matern32_kernel(x1, x2, params):
+    z1 = x1 / params["lengthscale"]
+    z2 = x2 / params["lengthscale"]
+    d = jnp.sqrt(3.0) * jnp.sqrt(jnp.sum((z1 - z2) ** 2))
+    return (1.0 + d) * jnp.exp(-d)
+
+
+def matern52_kernel(x1, x2, params):
+    z1 = x1 / params["lengthscale"]
+    z2 = x2 / params["lengthscale"]
+    d = jnp.sqrt(5.0) * jnp.sqrt(jnp.sum((z1 - z2) ** 2))
+    return (1.0 + d + d**2 / 3.0) * jnp.exp(-d)
 
 
 # =============================================================================
 # Kernel abbreviations
 # =============================================================================
 
-# Squared Exponential Kernel
 se_kernel = squared_exponential_kernel
 rbf_kernel = squared_exponential_kernel
+m12_kernel = matern12_kernel
+m32_kernel = matern32_kernel
+m52_kernel = matern52_kernel
