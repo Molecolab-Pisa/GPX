a34b9772db1cc4b40dbd53415fabfa38935923ae
Edoardo Cignoni <edoardo.cignoni96@gmail.com>
2022-11-28 18:26:30 +0100

    Added function to compute euclidean distances fast

--- gpx/utils.py
+++ gpx/utils.py
@@ -2,6 +2,19 @@ import jax.numpy as jnp
 
 
 # =============================================================================
+# Operations
+# =============================================================================
+
+
+def squared_distances(x1, x2):
+    jitter = 1e-12
+    x1s = jnp.sum(jnp.square(x1), axis=-1)
+    x2s = jnp.sum(jnp.square(x2), axis=-1)
+    dist = x1s[:, jnp.newaxis] - 2 * jnp.dot(x1, x2.T) + x2s
+    return jnp.clip(dist, jitter)
+
+
+# =============================================================================
 # Transformation Functions
 # =============================================================================
 
