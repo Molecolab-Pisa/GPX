bb98b89703f2936ddb5c849f3c7eb2712b496794
Edoardo Cignoni <edoardo.cignoni96@gmail.com>
2022-12-08 17:15:57 +0100

    Removed jnp.clip on the calculation of euclidean distances, which was totally destroying derivatives

--- gpx/utils.py
+++ gpx/utils.py
@@ -18,8 +18,8 @@ def squared_distances(x1, x2):
     jitter = 1e-12
     x1s = jnp.sum(jnp.square(x1), axis=-1)
     x2s = jnp.sum(jnp.square(x2), axis=-1)
-    dist = x1s[:, jnp.newaxis] - 2 * jnp.dot(x1, x2.T) + x2s
-    return jnp.clip(dist, jitter)
+    dist = x1s[:, jnp.newaxis] - 2 * jnp.dot(x1, x2.T) + x2s + jitter
+    return dist
 
 
 # =============================================================================
