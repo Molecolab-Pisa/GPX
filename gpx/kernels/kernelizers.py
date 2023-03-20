42c60af29196cf0eed44b3548f9662e720307125
Edoardo Cignoni <edoardo.cignoni96@gmail.com>
2023-03-20 20:13:07 +0100

    huge rewriting of derivative kernelizers + rewritten examples using derivative kernel

--- gpx/kernels/kernelizers.py
+++ gpx/kernels/kernelizers.py
@@ -66,197 +66,206 @@ def kernelize(kernel_func: Callable, lax: bool = True) -> Callable:
 
 
 # =============================================================================
-# Derivative Kernel Decorator
+# Derivative Kernel Decorator: low level decorators
 # =============================================================================
-# TODO: right now we have a `vmap` version of the kernelizer, and a `lax`
-#       version. The lax one is ok for big datasets. Still, it stores in memory
-#       the full derivative kernel before taking the product with the jacobian
-#       this is, in general, memory inefficient. A better way to implement this
-#       is to perform the matrix jacobian product directly inside the for loop
-#       of jax.lax. __implement this__.
-#       Maybe is ok to have a _grad0_kernelize function that yields the derivative
-#       kernel (in which case, the current shape is wrong and has to be changed)
-#       and a _grad0jacob_kernelize function that yields the derivative kernel
-#       multiplied by the jacobian, and leave to grad0_kernelize the duty to
-#       dispatch the kernelization according to a `with_jacob` boolean argument.
 
 
-def _grad0_kernelize(kernel_func: Callable, lax: bool = False) -> Callable:
+def _grad0_kernelize(kernel_func: Callable) -> Callable:
     kernel_func = jacrev(kernel_func, argnums=0)
 
-    if lax:
+    @functools.wraps(kernel_func)
+    @jit
+    def kernel(x1, x2, params):
+        n, nf = x1.shape
+        m, _ = x2.shape
+        gram = jnp.zeros((n * nf, m))
 
-        @functools.wraps(kernel_func)
-        @jit
-        def kernel(x1, x2, params):
-            n, nf = x1.shape
-            m, _ = x2.shape
-            gram = jnp.zeros((n, m, nf))
+        def update_row(i, gram):
+            def update_col(j, gram):
+                nabla_k = jnp.atleast_2d(kernel_func(x1[i], x2[j], params)).T
+                return jax.lax.dynamic_update_slice(
+                    gram, update=nabla_k, start_indices=(i * nf, j)
+                )
 
-            def update_row(i, gram):
-                def update_col(j, gram):
-                    return gram.at[i, j, :].set(kernel_func(x1[i], x2[j], params))
+            return jax.lax.fori_loop(0, m, update_col, gram)
 
-                return jax.lax.fori_loop(0, m, update_col, gram)
-
-            return jax.lax.fori_loop(0, n, update_row, gram)
-
-    else:
-
-        # vmap version is compatible with jacrev
-        kernel = kernelize(kernel_func, lax=False)
+        return jax.lax.fori_loop(0, n, update_row, gram)
 
     return kernel
 
 
-def _grad1_kernelize(kernel_func: Callable, lax: bool = False) -> Callable:
+def _grad0jac_kernelize(kernel_func: Callable) -> Callable:
+    kernel_func = jacrev(kernel_func, argnums=0)
+
+    @functools.wraps(kernel_func)
+    @jit
+    def kernel(x1, x2, params, jacobian):
+        n, _ = x1.shape
+        m, _ = x2.shape
+        _, _, jv = jacobian.shape
+
+        gram = jnp.zeros((n * jv, m))
+
+        def update_row(i, gram):
+            def update_col(j, gram):
+                nabla_k = kernel_func(x1[i], x2[j], params)
+                nabla_k = jnp.einsum("i,ij->ji", nabla_k, jacobian[i])
+                return jax.lax.dynamic_update_slice(
+                    gram, update=nabla_k, start_indices=(i * jv, j)
+                )
+
+            return jax.lax.fori_loop(0, m, update_col, gram)
+
+        return jax.lax.fori_loop(0, n, update_row, gram)
+
+    return kernel
+
+
+def _grad1_kernelize(kernel_func: Callable) -> Callable:
     kernel_func = jacrev(kernel_func, argnums=1)
 
-    if lax:
+    @functools.wraps(kernel_func)
+    @jit
+    def kernel(x1, x2, params):
+        n, _ = x1.shape
+        m, mf = x2.shape
+        gram = jnp.zeros((n, m * mf))
 
-        @functools.wraps(kernel_func)
-        @jit
-        def kernel(x1, x2, params):
-            n, _ = x1.shape
-            m, mf = x2.shape
-            gram = jnp.zeros((n, m, mf))
+        def update_row(i, gram):
+            def update_col(j, gram):
+                nabla_k = jnp.atleast_2d(kernel_func(x1[i], x2[j], params))
+                return jax.lax.dynamic_update_slice(
+                    gram, update=nabla_k, start_indices=(i, j * mf)
+                )
 
-            def update_row(i, gram):
-                def update_col(j, gram):
-                    return gram.at[i, j, :].set(kernel_func(x1[i], x2[j], params))
+            return jax.lax.fori_loop(0, m, update_col, gram)
 
-                return jax.lax.fori_loop(0, m, update_col, gram)
-
-            return jax.lax.fori_loop(0, n, update_row, gram)
-
-    else:
-
-        # vmap version is compatible with jacrev
-        kernel = kernelize(kernel_func, lax=False)
+        return jax.lax.fori_loop(0, n, update_row, gram)
 
     return kernel
 
 
-def _grad01_kernelize(kernel_func: Callable, lax: bool = True) -> Callable:
+def _grad1jac_kernelize(kernel_func: Callable) -> Callable:
+    kernel_func = jacrev(kernel_func, argnums=1)
+
+    @functools.wraps(kernel_func)
+    @jit
+    def kernel(x1, x2, params, jacobian):
+        n, _ = x1.shape
+        m, _ = x2.shape
+        _, _, jv = jacobian.shape
+
+        gram = jnp.zeros((n, m * jv))
+
+        def update_row(i, gram):
+            def update_col(j, gram):
+                nabla_k = kernel_func(x1[i], x2[j], params)
+                nabla_k = jnp.einsum("i,ij->ij", nabla_k, jacobian[j])
+                return jax.lax.dynamic_update_slice(
+                    gram, update=nabla_k, start_indices=(i, j * jv)
+                )
+
+            return jax.lax.fori_loop(0, m, update_col, gram)
+
+        return jax.lax.fori_loop(0, n, update_row, gram)
+
+    return kernel
+
+
+def _grad01_kernelize(kernel_func: Callable) -> Callable:
     kernel_func = jacfwd(jacrev(kernel_func, argnums=0), argnums=1)
 
-    if lax:
+    @functools.wraps(kernel_func)
+    @jit
+    def kernel(x1, x2, params):
+        n, nf = x1.shape
+        m, mf = x2.shape
+        gram = jnp.zeros((n * nf, m * mf))
 
-        @functools.wraps(kernel_func)
-        @jit
-        def kernel(x1, x2, params):
-            n, nf = x1.shape
-            m, mf = x2.shape
-            gram = jnp.zeros((n, m, nf, mf))
+        def update_row(i, gram):
+            def update_col(j, gram):
+                nabla_k = jnp.atleast_2d(kernel_func(x1[i], x2[j], params))
+                return jax.lax.dynamic_update_slice(
+                    gram, update=nabla_k, start_indices=(i * nf, j * mf)
+                )
 
-            def update_row(i, gram):
-                def update_col(j, gram):
-                    return gram.at[i, j, :, :].set(kernel_func(x1[i], x2[j], params))
+            return jax.lax.fori_loop(0, m, update_col, gram)
 
-                return jax.lax.fori_loop(0, m, update_col, gram)
-
-            return jax.lax.fori_loop(0, n, update_row, gram)
-
-    else:
-
-        # vmap version is compatile with jacrev, jacfwd
-        kernel = kernelize(kernel_func, lax=False)
+        return jax.lax.fori_loop(0, n, update_row, gram)
 
     return kernel
 
 
-def grad0_kernelize(k: Callable, lax: bool = True) -> Callable:
-    """Kernelizes the kernel k and makes a derivative kernel
+def _grad01jac_kernelize(kernel_func: Callable) -> Callable:
+    kernel_func = jacfwd(jacrev(kernel_func, argnums=0), argnums=1)
 
-    d/d0(k) = cov(w, y) with y = f(x) and w = d/dx(f)(x).
+    @functools.wraps(kernel_func)
+    @jit
+    def kernel(x1, x2, params, jacobian1, jacobian2):
+        n, _ = x1.shape
+        m, _ = x2.shape
+        _, _, j1v = jacobian1.shape
+        _, _, j2v = jacobian2.shape
 
-    Returns:
-    d0_kernel: derivative kernel with respect to the first argument
-    """
-    d0k = _grad0_kernelize(k, lax=lax)
+        gram = jnp.zeros((n * j1v, m * j2v))
 
-    def wrapper(x1, x2, params, jacobian):
-        """Derivative kernel with respect to the first argument.
+        def update_row(i, gram):
+            def update_col(j, gram):
+                nabla_k = kernel_func(x1[i], x2[j], params)
+                nabla_k = jnp.einsum(
+                    "ai,ab,bj->ij", jacobian1[i], nabla_k, jacobian2[j]
+                )
+                return jax.lax.dynamic_update_slice(
+                    gram, update=nabla_k, start_indices=(i * j1v, j * j2v)
+                )
 
-        d/d0(k) = cov(w, y) with y = f(x) and w = d/dx(f)(x).
-        """
-        # n, m, d = x1.shape[0], x2.shape[0], x1.shape[1]
-        gram = d0k(x1, x2, params)
-        gram = jnp.transpose(gram, axes=(2, 1, 0))
-        gram = jnp.einsum("ijk,ijl->jkl", jacobian, gram)
-        n, d, m = gram.shape
-        return jnp.reshape(gram, (n * d, m))
-        # return jnp.transpose(gram, axes=(2, 1, 0))
-        # gram = jnp.transpose(gram, axes=(0, 2, 1))
-        # return jnp.reshape(gram, (n * d, m))
+            return jax.lax.fori_loop(0, m, update_col, gram)
 
-    wrapper.__doc__ = grad0_kernelize.__doc__
+        return jax.lax.fori_loop(0, n, update_row, gram)
 
-    return wrapper
-
-
-def grad1_kernelize(k: Callable, lax: bool = True) -> Callable:
-    """Kernelizes the kernel k and makes a derivative kernel
-
-    d/d1(k) = cov(y, w) with y = f(x) and w = d/dx(f)(x).
-
-    Returns:
-    d1_kernel: derivative kernel with respect to the second argument
-    """
-    d1k = _grad1_kernelize(k, lax=lax)
-
-    def wrapper(x1, x2, params, jacobian):
-        """Derivative kernel with respect to the second argument.
-
-        d/d1(k) = cov(y, w) with y = f(x) and w = d/dx(f)(x).
-        """
-        # n, m, d = x1.shape[0], x2.shape[0], x1.shape[1]
-        gram = d1k(x1, x2, params)
-        gram = jnp.einsum("ijk,kjl->ijl", gram, jacobian)
-        m, n, d = gram.shape
-        return jnp.reshape(gram, (m, n * d))
-        # return jnp.reshape(gram, (n, m * d))
-
-    return wrapper
+    return kernel
 
 
-def grad01_kernelize(k: Callable, lax: bool = True) -> Callable:
-    """Kernelizes the kernel k and makes a derivative kernel
-
-    d^2/d0d1(k) = cov(w, w) with y = f(x) and w = d/dx(f)(x).
-
-    Returns:
-    d0d1_kernel: derivative kernel with respect to the first and
-                 second argument
-    """
-    d01k = _grad01_kernelize(k, lax=lax)
-
-    def wrapper(x1, x2, params, jacobian):
-        # n, m, d = x1.shape[0], x2.shape[0], x1.shape[1]
-        gram = d01k(x1, x2, params)
-        gram = jnp.transpose(gram, axes=(2, 0, 1, 3))
-        gram = jnp.einsum("ijk,ijlm,mln->jkln", jacobian, gram, jacobian)
-        d, n, m, e = gram.shape
-        return jnp.reshape(gram, (n * d, m * e))
-        # return jnp.transpose(gram, axes=(2, 0, 1, 3))
-        # gram = jnp.transpose(gram, axes=(0, 2, 1, 3))
-        # return jnp.reshape(gram, (n * d, m * d))
-
-    return wrapper
+# =============================================================================
+# Derivative Kernel Decorator: high level decorators
+# =============================================================================
 
 
-def grad_kernelize(argnums: Union[int, Tuple[int, int]], lax: bool = True) -> Callable:
+def grad0_kernelize(kernel_func: Callable, with_jacob: bool = False):
+    if with_jacob:
+        return _grad0jac_kernelize(kernel_func)
+    else:
+        return _grad0_kernelize(kernel_func)
+
+
+def grad1_kernelize(kernel_func: Callable, with_jacob: bool = False):
+    if with_jacob:
+        return _grad1jac_kernelize(kernel_func)
+    else:
+        return _grad1_kernelize(kernel_func)
+
+
+def grad01_kernelize(kernel_func: Callable, with_jacob: bool = False):
+    if with_jacob:
+        return _grad01jac_kernelize(kernel_func)
+    else:
+        return _grad01_kernelize(kernel_func)
+
+
+def grad_kernelize(
+    argnums: Union[int, Tuple[int, int]], with_jacob: bool = False
+) -> Callable:
     """Kernelizes the input kernel with respect to the dimension
     specified in argnums.
 
     Only argnums == 0, 1, (0, 1) is available.
     """
     if argnums == 0:
-        return partial(grad0_kernelize, lax=lax)
+        return partial(grad0_kernelize, with_jacob=with_jacob)
     elif argnums == 1:
-        return partial(grad1_kernelize, lax=lax)
+        return partial(grad1_kernelize, with_jacob=with_jacob)
     elif argnums == (0, 1):
-        return partial(grad01_kernelize, lax=lax)
+        return partial(grad01_kernelize, with_jacob=with_jacob)
     else:
         raise ValueError(
             f"argnums={argnums} is not valid. Allowed argnums: 0, 1, (0, 1)"
