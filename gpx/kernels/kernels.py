b659b3efb715f7a7c3d8a48923fecd1cc2522f94
p.mazzeo <p.mazzeo1@studenti.unipi.it>
2023-04-13 11:01:04 +0200

    Added squared_distances trick in matern kernels

--- gpx/kernels/kernels.py
+++ gpx/kernels/kernels.py
@@ -66,7 +66,20 @@ def matern12_kernel_base(
     return _matern12_kernel_base(x1, x2, lengthscale)
 
 
-matern12_kernel = kernelize(matern12_kernel_base)
+def _matern12_kernel(x1: ArrayLike, x2: ArrayLike, lengthscale: float) -> Array:
+    z1 = x1 / lengthscale
+    z2 = x2 / lengthscale
+    d2 = squared_distances(z1, z2)
+    d = jnp.sqrt(jnp.maximum(d2, 1e-36))
+    return jnp.exp(-d)
+
+
+@jit
+def matern12_kernel(
+    x1: ArrayLike, x2: ArrayLike, params: Dict[str, Parameter]
+) -> Array:
+    lengthscale = params["lengthscale"].value
+    return _matern12_kernel(x1, x2, lengthscale)
 
 
 # =============================================================================
@@ -89,7 +102,20 @@ def matern32_kernel_base(
     return _matern32_kernel_base(x1, x2, lengthscale)
 
 
-matern32_kernel = kernelize(matern32_kernel_base)
+def _matern32_kernel(x1: ArrayLike, x2: ArrayLike, lengthscale: float) -> Array:
+    z1 = x1 / lengthscale
+    z2 = x2 / lengthscale
+    d2 = squared_distances(z1, z2)
+    d = jnp.sqrt(3.0) * jnp.sqrt(jnp.maximum(d2, 1e-36))
+    return (1.0 + d) * jnp.exp(-d)
+
+
+@jit
+def matern32_kernel(
+    x1: ArrayLike, x2: ArrayLike, params: Dict[str, Parameter]
+) -> Array:
+    lengthscale = params["lengthscale"].value
+    return _matern32_kernel(x1, x2, lengthscale)
 
 
 # =============================================================================
@@ -112,7 +138,20 @@ def matern52_kernel_base(
     return _matern52_kernel_base(x1, x2, lengthscale)
 
 
-matern52_kernel = kernelize(matern52_kernel_base)
+def _matern52_kernel(x1: ArrayLike, x2: ArrayLike, lengthscale: float) -> Array:
+    z1 = x1 / lengthscale
+    z2 = x2 / lengthscale
+    d2 = squared_distances(z1, z2)
+    d = jnp.sqrt(5.0) * jnp.sqrt(jnp.maximum(d2, 1e-36))
+    return (1.0 + d + d**2 / 3.0) * jnp.exp(-d)
+
+
+@jit
+def matern52_kernel(
+    x1: ArrayLike, x2: ArrayLike, params: Dict[str, Parameter]
+) -> Array:
+    lengthscale = params["lengthscale"].value
+    return _matern52_kernel(x1, x2, lengthscale)
 
 
 # =============================================================================
@@ -120,7 +159,6 @@ matern52_kernel = kernelize(matern52_kernel_base)
 # =============================================================================
 
 se_kernel = squared_exponential_kernel
-rbf_kernel = squared_exponential_kernel
 m12_kernel = matern12_kernel
 m32_kernel = matern32_kernel
 m52_kernel = matern52_kernel
@@ -210,15 +248,21 @@ class Matern12(Kernel):
     def __init__(self) -> None:
         self._kernel_base = matern12_kernel_base
         super().__init__()
+        # faster version for evaluating k
+        self.k = matern12_kernel
 
 
 class Matern32(Kernel):
     def __init__(self) -> None:
         self._kernel_base = matern32_kernel_base
         super().__init__()
+        # faster version for evaluating k
+        self.k = matern32_kernel
 
 
 class Matern52(Kernel):
     def __init__(self) -> None:
         self._kernel_base = matern52_kernel_base
         super().__init__()
+        # faster version for evaluating k
+        self.k = matern52_kernel
