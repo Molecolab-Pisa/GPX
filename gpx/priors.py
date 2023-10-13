2a06bb2c72a9c8e47ab215c0d161e022ba385b24
Edoardo Cignoni <edoardo.cignoni96@gmail.com>
2023-10-13 14:37:26 +0200

    implemented gamma prior

--- gpx/priors.py
+++ gpx/priors.py
@@ -7,11 +7,17 @@ from typing import Any, Tuple
 import jax.numpy as jnp
 from jax import Array, random
 from jax._src import prng
+from jax.scipy.special import gammaln
 from jax.typing import ArrayLike
 
 # ============================================================================
 # Functions
 # ============================================================================
+
+
+#
+# Normal
+#
 
 
 def normal_sample(
@@ -43,8 +49,7 @@ def normal_logpdf(
     loc: ArrayLike = 0.0,
     scale: ArrayLike = 1.0,
 ):
-    """
-    Evaluates the log pdf of the normal distribution
+    """evaluates the log pdf of the normal distribution
 
         log p(θ) = - log(σ) - ½log(2π) - (1/(2σ²)) (θ - μ)²
 
@@ -60,6 +65,55 @@ def normal_logpdf(
         -jnp.log(scale)
         - 0.5 * jnp.log(2 * jnp.pi)
         - 0.5 * (1.0 / scale**2) * (x - loc) ** 2
+    )
+
+
+#
+# Gamma
+#
+
+
+def gamma_sample(
+    key: prng.PRNGKeyArray,
+    concentration: ArrayLike = 1.0,
+    rate: ArrayLike = 1.0,
+    shape: Tuple = (),
+    dtype: Any = jnp.float64,
+) -> Array:
+    """samples from a gamma distribution
+
+        p(θ) = (1 / Γ(α)) β^α θ^(α-1) e^(-βθ)
+
+    Args:
+        key: PRNGKey
+        concentration: α parameter (also known as shape)
+        rate: β parameter
+        shape: shape of the sample
+        dtype: dtype of the sample
+    """
+    return random.gamma(key=key, a=concentration, shape=shape, dtype=dtype) / rate
+
+
+def gamma_logpdf(
+    x: ArrayLike, concentration: ArrayLike = 1.0, rate: ArrayLike = 1.0
+) -> Array:
+    """evaluates the lof pdf of the gamma distribution
+
+        log p(θ) = α logβ - logΓ(α) + (α-1)logθ - βθ
+
+    Args:
+        x: input point
+        concentration: α parameter (also known as shape)
+        rate: β parameter
+
+    Returns:
+        logpdf: log probability density function evaluated at x
+    """
+    return (
+        concentration * jnp.log(rate)
+        - gammaln(concentration)
+        + (concentration - 1.0) * jnp.log(x)
+        - rate * x
     )
 
 
@@ -166,3 +220,65 @@ class NormalPrior(Prior):
 
     def logpdf(self, x: ArrayLike) -> Array:
         return normal_logpdf(x=x, loc=self.loc, scale=self.scale)
+
+
+class GammaPrior(Prior):
+    def __init__(self, concentration=1.0, rate=1.0, shape=(), dtype=jnp.float64):
+        self.shape = shape
+        self.dtype = dtype
+        # initialize after assigning dtype so that
+        # the class knows what dtype to use
+        self.concentration = concentration
+        self.rate = rate
+
+    @property
+    def concentration(self):
+        return self._concentration
+
+    @concentration.setter
+    def concentration(self, value: ArrayLike) -> None:
+        self._concentration = jnp.array(value, dtype=self.dtype)
+
+    @property
+    def rate(self):
+        return self._rate
+
+    @rate.setter
+    def rate(self, value: ArrayLike) -> None:
+        self._rate = jnp.array(value, dtype=self.dtype)
+
+    def __str__(self) -> str:
+        name = "Gamma"
+        concentration = self.concentration
+        rate = self.rate
+        return f"{name}({concentration}, {rate})"
+
+    def __repr__(self) -> str:
+        name = self.__class__.__name__
+        reprstr = f"{name}(concentration={self.concentration}, rate={self.rate},"
+        reprstr += f" shape={self.shape}, dtype={self.dtype})"
+        return reprstr
+
+    def __copy__(self) -> "GammaPrior":
+        return self.__class__(
+            concentration=self.concentration,
+            rate=self.rate,
+            shape=self.shape,
+            dtype=self.dtype,
+        )
+
+    def sample(self, key: prng.PRNGKeyArray) -> Array:
+        return gamma_sample(
+            key,
+            concentration=self.concentration,
+            rate=self.rate,
+            shape=self.shape,
+            dtype=self.dtype,
+        )
+
+    def logpdf(self, x: ArrayLike) -> Array:
+        return gamma_logpdf(
+            x=x,
+            concentration=self.concentration,
+            rate=self.rate,
+        )
