ef7bd96e02d206393071ffeca3a51a7fde6efa8d
Edoardo Cignoni <edoardo.cignoni96@gmail.com>
2023-12-05 16:45:33 +0100

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="GPX",
-    version="0.0.1",
+    version="0.1.0",
     url="https://github.com/ecignoni/GPX",
     author="Edoardo Cignoni, Amanda Arcidiacono, Patrizia Mazzeo",
     author_email="edoardo.cignoni96@gmail.com",
