61d3c917e81cae38e08330d78886931b1731fae6
Edoardo Cignoni <edoardo.cignoni96@gmail.com>
2024-03-12 09:40:37 +0100

    fixed deprecation warning on importing config from jax.config

--- gpx/__init__.py
+++ gpx/__init__.py
@@ -1,6 +1,6 @@
-from jax.config import config
+import jax
 
 from . import kernels, models, parameters
 
 # Always enable double precision when importing this module
-config.update("jax_enable_x64", True)
+jax.config.update("jax_enable_x64", True)
